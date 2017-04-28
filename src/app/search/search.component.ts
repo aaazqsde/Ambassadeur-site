c22cddc637cb9bfe75ccd1dbf7a926dfba7b8f60
Hanison <lenoir.guillaume@yahoo.fr>
2017-04-28 16:04:10 +0200

    Modif des routerLink qui sont maintenant dans le restau-thumbnail.

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -1,29 +1,35 @@
 import {Component, OnInit} from "@angular/core";
 import {Restaurant} from "../restaurant/restaurant";
 import {RESTAURANTS} from "../restaurant/mock-restaurant";
-@Component ({
+@Component({
   selector: 'app-search',
-  templateUrl : './search.component.html',
+  templateUrl: './search.component.html',
   styleUrls: ['./search.component.css']
 })
-export class SearchComponent implements OnInit{
+export class SearchComponent implements OnInit {
 
   search: string;
-  restaurants : Restaurant [] = [];
-  results : Restaurant [] = [];
+  restaurants: Restaurant [] = [];
+  results: Restaurant [] = [];
 
 
   ngOnInit() {
-  this.restaurants = RESTAURANTS;
-  this.compareName();
+    this.search = "";
+    this.restaurants = RESTAURANTS;
+    this.compareName();
 
   }
 
-  compareName (){
-    for (let resto of this.restaurants ){
-      if (resto.nom.includes(this.search)){
+  compareName() {
+    for (let resto of this.restaurants) {
+      if (resto.nom.toLowerCase().includes(this.search.toLowerCase())) {
         this.results.push(resto);
       }
     }
+  }
+
+
+  resetResults() {
+    this.results = [];
   }
 }
